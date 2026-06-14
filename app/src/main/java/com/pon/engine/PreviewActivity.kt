b021039472dacfe18526e180d552c7262dc22a26
osnailcyargta-ctrl <osnail.cyargta@gmail.com>
2026-06-14 11:29:17 +0700

    Add app/src/main/java/com/pon/engine/PreviewActivity.kt

---
+++ app/src/main/java/com/pon/engine/PreviewActivity.kt
@@ -0,0 +1,8 @@
+package com.pon.engine
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+class PreviewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
